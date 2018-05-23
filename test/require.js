1b34ae541dd1a1fd66b70b06ad5280776c9a62d6
Dexter Shepherd <dexterjshepherd@gmail.com>
2018-05-23 15:25:29 -0700

    change chirp to charli

--- test/require.js
+++ test/require.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
-const Chirp = require('../chirp');
+const Charli = require('../charli');
 
-describe('Chirp', function() {
+describe('Charli', function() {
   describe('#processRequires', function() {
     it('should return all required paths', async function() {
-      const input = await Chirp.loadFile('test/require/require.ck')
+      const input = await Charli.loadFile('test/require/require.ck')
       assert.deepEqual( 
-        await Chirp.processRequires(input, 'test/require/require.ck'),
+        await Charli.processRequires(input, 'test/require/require.ck'),
         [ 'test/require/class-1.ck', 
           'test/require/class-2.ck',
           'test/require/class-3.ck' ]
--- test/run.js
+++ test/run.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
-const Chirp = require('../chirp');
+const Charli = require('../charli');
 
 describe('Chirp', function() {
   describe('#run', function() {
     it('should run chuck', async function() {
-      const code = await Chirp.run('./test/run/run.ck', false)
+      const code = await Charli.run('./test/run/run.ck', false)
       assert.equal(code, 0)
     })
   })
